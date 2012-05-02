1f3c8b06e339fbe4bb3eca86b7c326fc64476685
seppen <seppen@mail.bg>
2012-05-03 02:27:00 +0300

    init

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'haml-rails'
+
+group :development, :test do
+	gem 'mysql2'
+	gem 'cucumber-rails'
+	gem 'database_cleaner'
+	gem 'capybara'
+	gem 'rspec-rails'
+end
+
+group :production do
+	gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TomatoSoup::Application.config.secret_token = '5fd1731e4819c4c1428017fa6397e16f237076938c9120c2c45557672312a77dc5f9042a602fb2cef1df9f4399971d4e09d071a2aef954da410a7bf867f79300'
