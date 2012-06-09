b5503d82041f0422676dbcbdcf9b746c358ec99f
seppen <seppen@mail.bg>
2012-06-09 03:50:06 +0300

    Fixed division by zero

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -14,14 +14,14 @@ class StatsController < ApplicationController
     sum = 0
     tmp = current_user.projects.active.total_working_time
     tmp.each {|row| sum += row[1]}
-    sum / tmp.count
+    tmp.count == 0 ? 0 : sum / tmp.count
   end
   
   def estimated_time_to_real_time_ratio_for_goals
     sum = 0;
-    tmp = current_user.projects.active.joins(:goals).sum("estimated_time/real_time", :group => "Goals.id")
+    tmp = current_user.projects.active.joins(:goals).sum("estimated_time/(real_time + 0.000001)", :group => "Goals.id")
     tmp.each {|row| sum += row[1].to_f}
-    sum / tmp.count
+    tmp.count == 0 ? 0 : sum / tmp.count
   end
   
   def total_working_time
